d637aa6bdc15180c1f29f573e6dcc1ab7bb09b7c
helmlingp <66004946+helmlingp@users.noreply.github.com>
2021-02-24 16:22:10 +1030

    Update Windows Defender Sensors.ps1
    added examples and descriptions of each

--- Windows Defender Sensors.ps1
+++ Windows Defender Sensors.ps1
@@ -8,11 +8,29 @@
 	  Filename:     	
 	.DESCRIPTION
 	  A bunch of Windows Defender related Sensors
-    .EXAMPLE
+	  See https://docs.microsoft.com/en-us/previous-versions/windows/desktop/defender/msft-mpthreatdetection for info on MSFT_MpThreatDetection class and return codes.
+
+	  defender_uptodate returns True if Windows Defender signature was last updated less than or equal to 1 day ago. You want this to be True!
+	  defender_outofdate7 returns True if Windows Defender signature was last updated less than or equal to 7 day ago and more than 1 day ago. 
+	  defender_outofdate14 returns True if Windows Defender signature was last updated less than or equal to 14 day ago and more than 7 day ago. 
+	  defender_threatremovalfailure returns True if any threat is detected and the status is either "CleanFailed", "QuarantineFailed", "RemoveFailed", "AllowFailed", "Abondoned", or "BlockedFailed"
+	  defender_reqreboot returns True if any threat is detected and any status requires Reboot, eg. is either "RebootRequired", "FullScanAndRebootRequired", "RebootAndManualStepsRequired", "FullScanAndRebootAndManualStepsRequired", "RebootAndOfflineScanRequired", "FullScanAndRebootAndOfflineScanRequired", "RebootAndManualStepsAndOfflineScanRequired", "FullScanAndRebootAndManualStepsAndOfflineScanRequired"
+	  defender_reqofflinescan returns True if any threat is detected and status is OfflineScanRequired
+	  defender_reqfullscan returns True if any threat is detected and status requires FullScan, eg. is either "FullScanRequired", "FullScanAndRebootRequired", "FullScanAndManualStepsRequired", "FullScanAndRebootAndManualStepsRequired", "FullScanAndOfflineScanRequired", "FullScanAndManualStepsAndOfflineScanRequired", "FullScanAndRebootAndManualStepsAndOfflineScanRequired"
       
 #>
 defender_uptodate
 $defender=(Get-Date).Subtract((Get-MpComputerStatus).AntispywareSignatureLastUpdated).TotalDays -LE 1
+return $defender
+ 
+ 
+defender_outofdate7
+$defender=(Get-Date).Subtract((Get-MpComputerStatus).AntispywareSignatureLastUpdated).TotalDays -GT 1 -and (Get-Date).Subtract((Get-MpComputerStatus).AntispywareSignatureLastUpdated).TotalDays -LE 7
+return $defender
+ 
+ 
+defender_outofdate14
+$defender=(Get-Date).Subtract((Get-MpComputerStatus).AntispywareSignatureLastUpdated).TotalDays -GT 7 -and (Get-Date).Subtract((Get-MpComputerStatus).AntispywareSignatureLastUpdated).TotalDays -LE 14
 return $defender
  
  
@@ -33,14 +51,4 @@ return $defender
  
 defender_reqfullscan
 $defender=((Get-MpThreatDetection | Where ThreatID -In (Get-MpThreat | Where IsActive -EQ $true | Select-Object -Property ThreatId).ThreatId | Where AdditionalActionsBitMask -IN @(4,12,20,28,32772,32788,32796)) | Measure).Count -GT 0
-return $defender
- 
- 
-defender_outofdate7
-$defender=(Get-Date).Subtract((Get-MpComputerStatus).AntispywareSignatureLastUpdated).TotalDays -GT 1 -and (Get-Date).Subtract((Get-MpComputerStatus).AntispywareSignatureLastUpdated).TotalDays -LE 7
-return $defender
- 
- 
-defender_outofdate14
-$defender=(Get-Date).Subtract((Get-MpComputerStatus).AntispywareSignatureLastUpdated).TotalDays -GT 7 -and (Get-Date).Subtract((Get-MpComputerStatus).AntispywareSignatureLastUpdated).TotalDays -LE 14
 return $defender
