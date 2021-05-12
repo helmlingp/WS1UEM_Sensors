fe87ce8e565b68f07c029efebe8101cde9f0605c
helmlingp <66004946+helmlingp@users.noreply.github.com>
2021-05-12 11:11:55 +0930

    Update getGeoLocation.ps1
    added examples and changed default Country value to return as the ISO3166-alpha2 short value

--- getGeoLocation.ps1
+++ getGeoLocation.ps1
@@ -31,15 +31,23 @@ $devicegeoCountry = $devicegeodata.Country
 $devicegeoRegion = $devicegeodata.Region
 $devicegeoCity = $devicegeodata.City
 
-#Return what you want - Country, Region or City
 #Refer to https://www.geonames.org/countries/ for $devicegeoCountry codes.
 #Return value should be used to find in WS1 Intelligence Automation and Tag a device. 
 #The Tag would be a filter in a SmartGroup which is used to assign a Profile/Application.
-#Example to return proper Country names rather than shortnames
-if ($devicegeoCountry -eq "AU") {return "Australia"}
-elseif ($devicegeoCountry -eq "US") {return "United States"}
-elseif ($devicegeoCountry -eq "IN") {return "India"}
 
+#example to return Country
+return $devicegeoCountry
+
+#Example to return proper Country names rather than shortnames
+#if ($devicegeoCountry -eq "AU") {return "Australia"}
+#elseif ($devicegeoCountry -eq "US") {return "United States"}
+#elseif ($devicegeoCountry -eq "IN") {return "India"}
+
+#example to return Region/State
 #return $devicegeoRegion
 
+#example to return City
 #return $devicegeoCity
+
+#example to return combination of all three attributes
+#$returnvalue = "$devicegeoCountry-$devicegeoRegion-$devicegeoCity"
