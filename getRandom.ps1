de86fd556405d7160b4311a561c35aab1b58fe0a
helmlingp <66004946+helmlingp@users.noreply.github.com>
2021-05-31 11:42:43 +0930

    Update getRandom.ps1
    added storing random number generated into registry, otherwise each time sensor runs we get a new number.

--- getRandom.ps1
+++ getRandom.ps1
@@ -1,5 +1,28 @@
-# Returns a random number between 1 and 3
-# Return Type: Integer
-# Execution Context: User
-$ring_number = Get-Random -Minimum 1 -Maximum 4
-write-output $ring_number
+<#	
+  .Synopsis
+  Returns a random number between 1 and 9
+  .NOTES
+  Created:	May, 2021
+  Created by:	Phil Helmling, @philhelmling
+  Organization:	VMware, Inc.
+
+  .DESCRIPTION
+  Use this sensor to write a random number to the registry if not already set
+  and return the value to WS1 Intelligence. 
+  Can be used to create dynamic SmartGroups for Windows Update Deployment Rings.
+  
+  Return Type: Integer
+  Execution Context: System
+#>
+
+$value = Get-Random -Minimum 1 -Maximum 10
+$name = "RandomNumber"
+$key = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Airwatch"
+if(Get-ItemProperty -Path $key -Name $name -ErrorAction Ignore) {
+  $returnvalue = (Get-ItemProperty -Path $key -Name $name).$name
+} else {
+  New-ItemProperty -Path $key -Name $name -PropertyType DWORD -Value $value -ErrorAction SilentlyContinue -Force
+  $returnvalue = (Get-ItemProperty -Path $key -Name $name).$name
+}
+
+return $returnvalue
