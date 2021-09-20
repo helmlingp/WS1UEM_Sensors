f511e59bccd12b1c4daa2be5026dc0996d11684b
helmlingp <66004946+helmlingp@users.noreply.github.com>
2021-09-21 08:41:44 +0930

    Update wu_status.ps1
    changed matching of available updates to updates that were previously installed and get status. If status equals failed, then return Update-Failed. Otherwise if there are available updates, return Out-of-Date

--- wu_status.ps1
+++ wu_status.ps1
@@ -7,6 +7,8 @@
 
 $Session = New-Object -ComObject Microsoft.Update.Session
 $UpdateSearcher = $Session.CreateUpdateSearcher()
+$UpdateHistory = $UpdateSearcher.QueryHistory(0,100)
+
 $Criteria = "IsHidden=0 and IsInstalled=0 and IsAssigned=1"
 $SearchResult = $UpdateSearcher.Search($Criteria).Updates
 
@@ -14,19 +16,23 @@ $Sysinfo = New-Object -ComObject Microsoft.Update.SystemInfo
 $pending = $Sysinfo.RebootRequired
 if($pending -eq $true){return "Pending Reboot"} 
 
-$failedstate = 0
-
+$status = 0
 if ($SearchResult.Count -ne 0) {
-  $SearchResult | ForEach-Object {
-    $UpdateID = $_.Identity.UpdateID
-    $UpdateIdQuery = "UpdateID='$UpdateID'"
-    $query = $session.QueryHistory("$UpdateIdQuery",0,100)
-    #match Updates not installed with previous install status if exists
-    $failed = $query | Where-Object {$_.ResultCode -eq 4} | Sort-Object -Property @{Expression = "Date"; Descending = $true} | Select-Object -First 1 
-    if ($failed) {
-      $failedstate = 1
+  foreach ($newupd in $SearchResult) {
+    $UpdateID = $newupd.Identity.UpdateID
+    $updhistoryitem = $updhistory | Where-Object {$_.UpdateIdentity.UpdateID -match $UpdateID}
+    if($updhistoryitem){
+      if($updhistoryitem.ResultCode -eq 4) {$status = 4}
     }
   }
-  if($failedstate -eq 1){return "Update-Failed"} else {return "Out-of-Date"}
 }
-if ($SearchResult -eq 0){return "Up-to-Date"}
+
+if ($SearchResult.Count -eq 0){
+  return "Up-to-Date"
+}else{
+  if($status -eq 4){
+    return "Update-Failed"
+  } else {
+    return "Out-of-Date"
+  }
+}
